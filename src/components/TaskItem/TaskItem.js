7e4dcf37d8a9258820b60d2998708201852c9446
comconToukir <ahmdtoukir@gmail.com>
2022-12-26 23:39:30 +0600

    design revamped

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Paper } from '@material-ui/core';
 
 
 
@@ -12,14 +13,23 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: 2,
   },
-  grid: {
-    width: "100%",
+  gridItem: {
+    padding: 12,
+    boxShadow: "2px 2px 3px lightgray",
+    marginBottom: 15,
+    border: "1px solid lightgray",
+    borderRadius: 3,
+
+    "&:hover": {
+    border: "1px solid black",
+    }
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+
 }));
 
 
@@ -29,17 +39,17 @@ const TaskItem = ({ item }) => {
   const classes = useStyles();
 
   return (
-      <Grid container justifyContent='space-between' style={{padding: 12}}>
-        <FormControlLabel control={<Checkbox name="checkedC" />} label={item} />
-        <Grid>
-          <Button variant="contained" color="primary" style={{marginRight: 8}}>
-            Edit
-          </Button>
-          <Button variant="contained" color="primary">
-            Delete
-          </Button>
-        </Grid>
+    <Grid item container justifyContent='space-between' className={classes.gridItem} >
+      <FormControlLabel control={<Checkbox name="checked" color="primary" />} label={item} />
+      <Grid container alignItems='center' style={{ width: "max-content"}}>
+        <Button variant="contained" color="primary" style={{ marginRight: 8 }}>
+          Edit
+        </Button>
+        <Button variant="contained" color="secondary">
+          Delete
+        </Button>
       </Grid>
+    </Grid>
   );
 };
 
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,13 +1,15 @@
+import { Box } from "@material-ui/core";
 import React from "react";
 import TaskItem from "../TaskItem/TaskItem";
+import Grid from '@material-ui/core/Grid';
 
 const TaskList = () => {
   return (
-    <div>
+    <Grid container direction="row" style={{padding: 9}}>
       {[1, 2, 3, 4].map((item, i) => {
         return <TaskItem key={i} item={item} />;
       })}
-    </div>
+    </Grid>
   );
 };
 
